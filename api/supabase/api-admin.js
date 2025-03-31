f38705cdef07fa2a37ccd606929f83976816a110
kelvin-fumador <kelvinfumador04@gmail.com>
2025-03-31 19:54:25 +0000

    Token verification test 3

--- api/supabase/api-admin.js
+++ api/supabase/api-admin.js
@@ -7,20 +7,24 @@ const verify_request_token = async (request_token, callback) => {
     .select("id, token, token_status, order_status")
     .eq("order_status", "processed");
 
+  console.log(arr);
+
   const newArr = arr?.filter((item) =>
     item?.token === request_token ? true : false
   );
 
+  console.log(newArr);
+
   if (newArr?.length > 0) {
     const obj = newArr[0];
-    if (obj?.token_status === "unverified") {
+    console.log(obj);
+    if (obj.token_status === "unverified") {
       const { error } = await options
         .update({ token_status: "verified" })
-        .eq("id", obj?.id);
+        .eq("id", obj.id);
 
       if (error) {
-        callback("Token verification error, try again!", null);
-        return;
+        return callback("Token verification error, try again!", null);
       }
 
       callback(null, { status: "current_verification" });
